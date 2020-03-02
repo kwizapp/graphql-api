79c344cfa34165d5107ade95bbf94cea6e5451cb
Roland Schläfli <rolandschlaefli@gmail.com>
2020-03-02 13:34:17 +0100

    Foundations for a NestJs App (#1)
    * Add Nest foundations from legacy project
    
    * Add graphql schema

---
+++ src/movie/movie.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common'
+
+import { Movie } from '../graphql.schema'
+import movies from './movies'
+
+@Injectable()
+export class MovieService {
+  getMovies(): Movie[] {
+    return movies
+  }
+
+  getMovieByIndex(index: number) {
+    try {
+      return movies[index]
+    } catch (e) {
+      return null
+    }
+  }
+
+  upVoteMovie(movieId: string) {
+    const movie = movies.find(m => m.id === movieId)
+    movie.votes++
+    return movie
+  }
+}
