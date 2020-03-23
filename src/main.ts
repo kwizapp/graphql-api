d9c1ef240cb55fae8a2523378fa64ba219cfd8e0
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2020-03-23 06:57:26 +0100

    #26: use dotenv to configure poster/metadata service location (#32)
    * #26: use dotenv to configure poster/metadata service location
    
    * pin dotenv dependecy -> remove caret

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
-  await app.listen(process.env.PORT || 3000)
+  await app.listen(PORT)
 }
 bootstrap()
+
+console.log(`The API runs on port:\t\t\t${PORT}`)
+console.log(
+  `The POSTER SERVICE is located at:\t${process.env.POSTER_SERVICE_URL}`,
+)
+console.log(
+  `The METADATA SERVICE is located at:\t${process.env.METADATA_SERVICE_URL}`,
+)
